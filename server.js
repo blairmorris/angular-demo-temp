7d33966fb472c5f1d7c00ee82bde5d27d8ae70aa
Blair Morris <bmorris1@consultant.underarmour.com>
2015-12-08 21:33:14 -0500

    Rewrite the hosting side of the app, grunt was total overkill for such a small app. Also fixed heroku hosting.

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var app = express();
 var open = require('open');
 var path = require('path');
 
+app.set('port', (process.env.PORT || 65001));
+app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use('/', express.static(__dirname + '/app'));
 
-app.listen(65001);
-open("http://localhost:65001");
+app.listen(app.get('port'), function() {
+	console.log('App is running on port ', app.get('port'));
+});
+open("http://localhost:" + app.get('port'));
